84f28bfa70f4691da360bbc4f7f637fc5d6bbe27
Lucas Pimentel <lpimentel@clearcompany.com>
2017-05-01 18:22:27 -0400

    fix route template

--- cc-slack-api/Controllers/SlackController.cs
+++ cc-slack-api/Controllers/SlackController.cs
@@ -4,7 +4,7 @@ namespace cc_slack_api.Controllers
 {
     public class SlackController : ApiController
     {
-        [Route("/slack/url_verification")]
+        [Route("slack/url_verification")]
         public IHttpActionResult UrlVerification(dynamic data)
         {
             if (data.token == "Jhj5dZrVaK7ZwHHjRyZWjbDl" && data.type == "url_verification")
