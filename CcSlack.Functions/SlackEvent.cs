fa6d3d5c014883399456a2e1d330babbecf418aa
Lucas Pimentel <lucaspimentel@gmail.com>
2018-02-03 11:25:42 -0500

    implement the Function

--- CcSlack.Functions/SlackEvent.cs
+++ CcSlack.Functions/SlackEvent.cs
@@ -1,7 +1,8 @@
-using System.Linq;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using CcSlack.Shared.Slack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -11,8 +12,11 @@ namespace CcSlack.Functions
     public static class SlackEvent
     {
         [FunctionName("SlackEvent")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "slack/event")]
+                                                          HttpRequestMessage req,
+                                                          TraceWriter log)
         {
+            /*
             log.Info("C# HTTP trigger function processed a request.");
 
             // parse query parameter
@@ -29,6 +33,46 @@ namespace CcSlack.Functions
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
                 : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+                */
+
+            dynamic args = await req.Content.ReadAsAsync<object>();
+
+            if (args == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "No data");
+            }
+
+            string token = args.token;
+            string type = args.type;
+
+            if (token == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "No token");
+            }
+
+            if (token != ConfigurationManager.AppSettings["slack_verification_token"])
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Wrong token");
+            }
+
+            if (type == "url_verification")
+            {
+                string challenge = args.challenge;
+
+                if (string.IsNullOrWhiteSpace(challenge))
+                {
+                    return req.CreateResponse(HttpStatusCode.BadRequest, "No challenge");
+                }
+
+                return req.CreateResponse(HttpStatusCode.OK, challenge);
+            }
+
+            if (type == "event_callback")
+            {
+                Task.Run(() => SlackEventHandler.HandleEvent((object)args));
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
